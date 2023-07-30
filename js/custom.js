efbf0471cbe7a9a814c62fe1033194d09b797233
asdas <95152341+1213212132@users.noreply.github.com>
2023-07-30 15:55:48 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -18,71 +18,40 @@ $(function () {
     })
 
 
-    const MainSlide = new Swiper('.main_slide', {
-        loop: true,
-        parallax: true,
-        speed: 1600,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        on: {
-            slideChangeTransitionStart: function () {
-                $('.MainVisual .dots li')
-                    .eq(this.realIndex)
-                    .addClass('on')
-                    .siblings()
-                    .removeClass('on')
-            }
-        }
-    });
 
 
 
 
-    $('.MainVisual .arrows .left').on('click', function () {
-        MainSlide.slidePrev();
-    });
-    $('.MainVisual .arrows .right').on('click', function () {
-        MainSlide.slideNext();
-    });
-
-
-    $('.MainVisual .dots li').on('click', function () {
-        const idx = $(this).index();
-        $(this).addClass('on').siblings().removeClass('on')
-        MainSlide.slideTo(idx);
-    });
-
-
-    const ItmSlide = new Swiper('.itm_slide', {
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 0,
-        speed: 900,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-
-        breakpoints: {
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 400,
-            }
-        }
-    });
-
-    $('.MainItm .arrows .left').on('click', function () {
-        ItmSlide.slidePrev();
-    });
-    $('.MainItm .arrows .right').on('click', function () {
-        ItmSlide.slideNext();
+    $('.tap_box .top').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).index();
+        $('.tap_on .bottom').eq(idx).addClass('on').siblings().removeClass('on');
+        $(this).addClass('on').siblings().removeClass('on');
     });
 
     // document.querySelector('.MainItm .arrows .left').addEventListener('click', () => {
     //     ItmSlide.slidePrev();
     // });
+
+    $('.news_slide').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    $('.tv_slide').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    $('.social_slide').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
 
     $('.mobile_btn').on('click', function () {
         $(this).toggleClass('on');
